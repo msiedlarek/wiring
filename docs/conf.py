7dd4f87a3c2b3fcaac0c7fcc123ca3c9f69c5a71
Mikołaj Siedlarek <mikolaj@siedlarek.pl>
2014-09-06 14:10:11 +0200

    Default Read the Docs theme for documentation.

--- docs/conf.py
+++ docs/conf.py
@@ -21,6 +21,9 @@ import os
 here = os.path.dirname(__file__)
 sys.path.insert(0, os.path.abspath(os.path.join(here, '..')))
 
+# on_rtd is whether the docs are being built on readthedocs.org
+on_rtd = (os.environ.get('READTHEDOCS', None) == 'True')
+
 import wiring
 
 # -- General configuration ------------------------------------------------
@@ -105,7 +108,8 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'nature'
+if not on_rtd:
+    html_theme = 'nature'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
